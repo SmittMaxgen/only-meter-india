ae8f879cd1dc2bec0e5436350fda8a6f2120acd2
Your Name <your.email@example.com>
2026-01-27 15:17:07 +0530

    DB : Port updated !! , Server script added !

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const db = {};
 const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize("ride_db", "root", "", {
   host: "localhost",
-  port: "3308",
+  port: "3306",
   dialect: "mysql",
 });
 
